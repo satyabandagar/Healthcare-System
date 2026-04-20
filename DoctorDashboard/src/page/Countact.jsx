948a4942597f49fddf76f573b6500982ef9023c4
satyabandagar <bandagarsatyappa8767@gmail.com>
2026-04-21 00:07:03 +0530

    doctor contact add

--- DoctorDashboard/src/page/Countact.jsx
+++ DoctorDashboard/src/page/Countact.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import "./Countact.css"
 
 function Countact() {
   return (
     <div>
-      <div className="">
-        <div className=""></div>
-        <div className=""></div>
+      <div className="DocContact">
+        <div className="DoctorHelp">
+          <form action="">
+            <label htmlFor="" style={{marginRight:"420px"}}>Full name</label>
+            <input type="text" placeholder='Enter your name' />
+            <label htmlFor="" style={{marginRight:"450px"}}>Email</label>
+            <input type="gmail" placeholder='Enter your gmail' />
+            <label htmlFor="">Problem Description</label>
+            <input type="text" />
+            <br />
+            <button type='submit'> Submit</button>
+          </form>
+        </div>
+        <div className="DoctoDes">
+          <h1>Version</h1>
+          <p>If you experience any issues while using this website, you can submit your problem here. Our team will carefully review your concern and take the necessary steps to resolve it as quickly as possible. We are committed to providing a smooth and reliable user experience.</p>
+        </div>
       </div>
     </div>
   )
